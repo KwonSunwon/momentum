14735a1ae74c73237ac305f21651f0145edbe02c
KwonSunwon <kso00011147@gmail.com>
2021-12-01 01:34:52 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "e8a831aedf4aef4acc8ba40a649e540d";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log(lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(Response => Response.json())
